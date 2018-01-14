a779f251729d6c52aa5eed35b771b34b58869bd9
Ian Leocádio <ian.c.leocadio@gmail.com>
2018-01-14 18:22:40 -0200

    Mensagem de sucesso do usuario

--- accounts/views.py
+++ accounts/views.py
@@ -14,7 +14,8 @@ def add_user(request):
             u = form.save()
             u.set_password(u.password)
             u.save()
-            return HttpResponse('Usuário criado com sucesso!')
+            messages.success(request, 'Usuário criado com sucesso. Utilize o formulário abaixo para fazer login')
+            return redirect('accounts:login_user')
     else:
         form = CreateUserForm()
 
